35cbd730289e891c4b06a6a4551317faf75cef09
JordanMoffat <jordanpmoffat@gmail.com>
2015-06-27 21:38:45 +0100

    Designed login screen

--- app/src/main/java/io/moffat/kitchenpal/Product.java
+++ app/src/main/java/io/moffat/kitchenpal/Product.java
@@ -1,10 +1,12 @@
 package io.moffat.kitchenpal;
 
+import java.util.Date;
+
 /**
  * Created by Jordan on 27/06/2015.
  */
 public class Product {
-    string ISBN;
-    DateTime expiry;
+    String ISBN = "";
+    Date expiry = null;
 
 }
