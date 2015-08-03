ca89a6a32383f777dc3dbb5a1d311f18f4360887
JordanMoffat <jordanpmoffat@gmail.com>
2015-08-03 19:01:47 +0100

    Added functionality to add item
    Basic add, needs to have additional fields and parameters added

--- app/src/main/java/io/moffat/kitchenpal/AddItem.java
+++ app/src/main/java/io/moffat/kitchenpal/AddItem.java
@@ -1,7 +1,9 @@
 package io.moffat.kitchenpal;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -11,7 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toolbar;
+import android.widget.Toast;
+
 
 import com.parse.*;
 
@@ -19,48 +22,68 @@ import java.util.Date;
 
 public class AddItem extends ActionBarActivity {
 
-    EditText editText;
-    EditText ISDN;
-    EditText ExpiryDate;
+
+    private Toolbar toolbar;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
 
-        Spinner spinnercategory = (Spinner)findViewById(R.id.spinnercategory);
+
+
+    final EditText ProductName;
+       final EditText ISDN_text;
+        final EditText expiry_date;
+
+
+
+        setContentView(R.layout.activity_add_item);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Add Item");
+
+    //    Spinner spinnercategory = (Spinner)findViewById(R.id.spinnercategory);
         String[] items = new String[]{"Meat", "Vegetables", "Ready Meal", "Juice", "Milk", "Leftovers"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-        spinnercategory.setAdapter(adapter);
+     //   ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+      //  spinnercategory.setAdapter(adapter);
+
+        ProductName = (EditText) findViewById(R.id.ProductName);
+        ISDN_text = (EditText)findViewById(R.id.ISDN);
+        expiry_date = (EditText)findViewById(R.id.expiry_date);
 
         Button save = (Button) findViewById(R.id.btnSave);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                ParseObject Product = new ParseObject("Product");
-
-          EditText nameText = (EditText) findViewById(R.id.editText);
-             String nameString = editText.getText().toString();
-
-             EditText barcodeText = (EditText) findViewById(R.id.ISDN);
-             String barcodeString = ISDN.getText().toString();
 
 
-                Product.put("productname", nameString);
-                Product.put("ISDN", barcodeString);
-                Product.put("Expiry", "24-8-2015T00:00:00Z");
-                Product.put("Type", "Liquid");
-                Product.put("Username", "Admin");
-                Product.saveInBackground();
-                //Product.put();
+                ParseObject newProduct = new ParseObject("Product");
 
 
+                newProduct.put("productName", ProductName.getText().toString());
+                newProduct.put("ISDN", ISDN_text.getText().toString());
+                newProduct.put("expiry", expiry_date.getText().toString());
+                newProduct.put("type", "Liquid");
+                newProduct.put("username", "Admin");
+                newProduct.put("listFlag", "main");
+                newProduct.put("shoppingList", false);
+                newProduct.put("eaten", false);
+                newProduct.put("discarded", false);
+                newProduct.saveInBackground();
 
-                Intent i = new Intent(AddItem.this, MainActivity.class);
-                startActivity(i);
+
+                String message = ProductName.getText().toString() + " added to list";
+
+                Toast.makeText(getApplicationContext(), message,
+                    Toast.LENGTH_SHORT).show();
+
+                kill();
+
+
             }
         });
 
@@ -76,6 +99,12 @@ public class AddItem extends ActionBarActivity {
         //don't really need anything in here, except colours. add barcode icon when i can
         getMenuInflater().inflate(R.menu.menu_add_item, menu);
         return true;
+    }
+    public void kill(){
+        Intent i = new Intent(this, MainActivity.class);
+       // i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+        //finish();
     }
 
     @Override
@@ -94,35 +123,5 @@ public class AddItem extends ActionBarActivity {
     }
 
 
-    public void onClick(){
 
-
-    ParseObject SaveData = new ParseObject("Save");
-
-  //      EditText nameText = (EditText) findViewById(R.id.editText);
-    //    String nameString = editText.getText().toString();
-
-     //   EditText barcodeText = (EditText) findViewById(R.id.ISDN);
-      //  String barcodeString = ISDN.getText().toString();
-
-   //     EditText expiryText = (EditText) findViewById(R.id.ExpiryDate);
-    //    int day = ExpiryDate.getDayOfMonth();
-
-      //  Date exdt = new Date(ExpiryDate.getDa)
-     //   Date expireDate = ExpiryDate.getText().
-
-
-
-
-
-
-       // String nametext = (EditText) findViewById(R.id.editText);
-
-     //   String prodname = editText.getText().ToString();
-
-        //get info from boxes and set to parse object
-        //also might need to get an actionlistener for the class types
-        //save item here
-
-    }
 }
