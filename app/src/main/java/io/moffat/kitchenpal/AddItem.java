5ea8c5fae9e7dc67d7f690fa8cc9800ffa8f0d4b
JordanMoffat <jordanpmoffat@gmail.com>
2015-08-04 17:57:01 +0100

    Added form items
    Added datepicker dialog and spinnerdialog, and clicklistener for API
    search

--- app/src/main/java/io/moffat/kitchenpal/AddItem.java
+++ app/src/main/java/io/moffat/kitchenpal/AddItem.java
@@ -1,6 +1,7 @@
 package io.moffat.kitchenpal;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -18,26 +20,22 @@ import android.widget.Toast;
 
 import com.parse.*;
 
+import java.text.*;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class AddItem extends ActionBarActivity {
 
 
     private Toolbar toolbar;
-
+    private Date expiry;
+    Calendar myCalendar = Calendar.getInstance();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-    final EditText ProductName;
-       final EditText ISDN_text;
-        final EditText expiry_date;
-
-
 
         setContentView(R.layout.activity_add_item);
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
@@ -45,51 +43,7 @@ public class AddItem extends ActionBarActivity {
 
         getSupportActionBar().setTitle("Add Item");
 
-    //    Spinner spinnercategory = (Spinner)findViewById(R.id.spinnercategory);
-        String[] items = new String[]{"Meat", "Vegetables", "Ready Meal", "Juice", "Milk", "Leftovers"};
-     //   ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-      //  spinnercategory.setAdapter(adapter);
-
-        ProductName = (EditText) findViewById(R.id.ProductName);
-        ISDN_text = (EditText)findViewById(R.id.ISDN);
-        expiry_date = (EditText)findViewById(R.id.expiry_date);
-
-        Button save = (Button) findViewById(R.id.btnSave);
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-
-                ParseObject newProduct = new ParseObject("Product");
-
-
-                newProduct.put("productName", ProductName.getText().toString());
-                newProduct.put("ISDN", ISDN_text.getText().toString());
-                newProduct.put("expiry", expiry_date.getText().toString());
-                newProduct.put("type", "Liquid");
-                newProduct.put("username", "Admin");
-                newProduct.put("listFlag", "main");
-                newProduct.put("shoppingList", false);
-                newProduct.put("eaten", false);
-                newProduct.put("discarded", false);
-                newProduct.saveInBackground();
-
-
-                String message = ProductName.getText().toString() + " added to list";
-
-                Toast.makeText(getApplicationContext(), message,
-                    Toast.LENGTH_SHORT).show();
-
-                kill();
-
-
-            }
-        });
-
-
-
-
+        onCreateHandler();
 
     }
 
@@ -119,7 +73,118 @@ public class AddItem extends ActionBarActivity {
             return true;
         }
 
+
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.UK);
+
+        EditText update = (EditText)findViewById(R.id.expiry_date);
+
+        update.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    public void onCreateHandler(){
+
+        final EditText ProductName;
+        final EditText ISDN_text;
+        final EditText expiry_date;
+
+        final Spinner spinnercategory = (Spinner)findViewById(R.id.spinnercategory);
+        String[] items = new String[]{"Meat", "Vegetables", "Ready Meal", "Juice", "Milk", "Leftovers"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        spinnercategory.setAdapter(adapter);
+
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        ProductName = (EditText) findViewById(R.id.ProductName);
+        ISDN_text = (EditText)findViewById(R.id.ISDN);
+        expiry_date = (EditText)findViewById(R.id.expiry_date);
+
+        expiry_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(AddItem.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+        });
+
+        Button save = (Button) findViewById(R.id.btnSave);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String dateString = expiry_date.getText().toString();
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                Date convertedDate = new Date();
+
+                try{
+                    convertedDate = dateFormat.parse(dateString);
+                } catch (java.text.ParseException e){
+                    e.printStackTrace();
+                }
+
+                ParseObject newProduct = new ParseObject("Product");
+
+
+                newProduct.put("productName", ProductName.getText().toString());
+                newProduct.put("ISDN", ISDN_text.getText().toString());
+                newProduct.put("expiry", convertedDate);
+                newProduct.put("type", spinnercategory.getSelectedItem().toString());
+                newProduct.put("username", "Admin");
+                newProduct.put("listFlag", "main");
+                newProduct.put("shoppingList", false);
+                newProduct.put("eaten", false);
+                newProduct.put("discarded", false);
+                newProduct.saveInBackground();
+
+
+                String message = ProductName.getText().toString() + " added to list";
+
+                Toast.makeText(getApplicationContext(), message,
+                        Toast.LENGTH_SHORT).show();
+
+                kill();
+
+
+            }
+        });
+
+        Button search = (Button)findViewById(R.id.btnSearch);
+            search.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+                String message = "Search Button Clicked";
+                Toast.makeText(getApplicationContext(), message,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
 
