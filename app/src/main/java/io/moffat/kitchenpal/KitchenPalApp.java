26812b5b7cacf7f1540329a6db26abb568cb7d3b
JordanMoffat <jordanpmoffat@gmail.com>
2015-08-08 22:31:34 +0100

    Changed initialisation settings to prevent parse bug

---
+++ app/src/main/java/io/moffat/kitchenpal/KitchenPalApp.java
@@ -0,0 +1,18 @@
+package io.moffat.kitchenpal;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by Jordan on 08/08/2015.
+ */
+public class KitchenPalApp extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+       // Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "fKFcxAML9tqkehYq0UxpMhkjSLIVdZEgIXTNLEtt", "poN3brKJyOkYPNZQWlCHMbzWHJAZIW4TMzQETGoJ");
+    }
+
+}
