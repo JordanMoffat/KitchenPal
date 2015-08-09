605fe69b9ee307b1a9f8295377bb4817b35514d4
JordanMoffat <jordanpmoffat@gmail.com>
2015-08-09 22:32:14 +0100

    Changed URL builder for different API library

--- app/src/main/java/io/moffat/kitchenpal/URLBuilder.java
+++ app/src/main/java/io/moffat/kitchenpal/URLBuilder.java
@@ -8,9 +8,7 @@ public class URLBuilder {
     String url = new String();
     public String builtURL(String code){
 
-        url = "http://api.upcdatabase.org/json/38231fc5c2fca1055f13a18306c4c817/" + code;
-
-
+        url = "http://api.ean-search.org/api?token=jord525723&op=barcode-lookup&format=json&ean" + code;
         return url;
     }
     //http://api.upcdatabase.org/json/38231fc5c2fca1055f13a18306c4c817/
