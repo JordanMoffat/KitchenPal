dcf8aa4e64c8e94196b0d9d933cb6984523fe6ca
JordanMoffat <jordanpmoffat@gmail.com>
2015-09-03 21:19:06 +0100

    Added some basic logic, need to add error handling

--- app/src/main/java/io/moffat/kitchenpal/Register.java
+++ app/src/main/java/io/moffat/kitchenpal/Register.java
@@ -8,11 +8,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.text.ParseException;
 
 
 public class Register extends ActionBarActivity {
 
+    final EditText username = (EditText) findViewById(R.id.editTextUsername);
+    final EditText password = (EditText) findViewById(R.id.password);
+    final EditText confirmPassword = (EditText) findViewById(R.id.confirmEmail);
+    final EditText email = (EditText) findViewById(R.id.emailText);
+    final EditText forename = (EditText) findViewById(R.id.editTextForename);
+    final EditText surname = (EditText) findViewById(R.id.editTextSurname);
     private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +36,49 @@ public class Register extends ActionBarActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+
         Button registery = (Button) findViewById(R.id.btnRegister);
         registery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
+                if (passwordMatch() == true){
+                    if (validEmail() == true) {
+                        ParseUser user = new ParseUser();
+                        user.setUsername(username.getText().toString());
+                        user.setPassword(password.getText().toString());
+                        user.setEmail(email.getText().toString());
+
+                        user.put("forename", forename.getText().toString());
+                        user.put("surname", surname.getText().toString());
+
+                        user.signUpInBackground(new SignUpCallback() {
+                            @Override
+                            public void done(com.parse.ParseException e) {
+                                if (e == null) {
+                                    Intent i = new Intent(Register.this, LoginActivity2.class);
+                                    startActivity(i);
+                                } else {
+                                    //error handling here
+                                }
+
+                            }
+                        });
+                    } else {
+                        //do feedback here
+                    }
+                } else {
+                    //do feedback here
+                }
+
+
+
                 //start progress spinner
                 //save stuff here
 
                 //success
-                Intent i = new Intent(Register.this, LoginActivity2.class);
-                startActivity(i);
+
                 //else
                 //highlight errors
             }
@@ -60,5 +105,25 @@ public class Register extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean passwordMatch(){
+
+        if (password.getText().toString() == confirmPassword.getText().toString()){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean validEmail(){
+
+        if (email.getText().toString().contains("@")){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
