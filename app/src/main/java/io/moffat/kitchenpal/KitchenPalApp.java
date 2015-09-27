dfeefbd699d90d238f29d58fedf9c7ae1597ac6b
JordanMoffat <jordanpmoffat@gmail.com>
2015-09-27 01:05:45 +0100

    Added Installation for Push

--- app/src/main/java/io/moffat/kitchenpal/KitchenPalApp.java
+++ app/src/main/java/io/moffat/kitchenpal/KitchenPalApp.java
@@ -3,16 +3,20 @@ package io.moffat.kitchenpal;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
 
 /**
  * Created by Jordan on 08/08/2015.
  */
 public class KitchenPalApp extends Application {
-    @Override
+
     public void onCreate(){
-        super.onCreate();
+    //    super.onCreate();
 
         Parse.initialize(this, "fKFcxAML9tqkehYq0UxpMhkjSLIVdZEgIXTNLEtt", "poN3brKJyOkYPNZQWlCHMbzWHJAZIW4TMzQETGoJ");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParsePush.subscribeInBackground("Expires");
     }
 
 }
